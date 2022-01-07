256ef40e8e9944253a5d678ed46b90d3d49a1766
Ethan Chandler <86173119+IzzTinkr@users.noreply.github.com>
2022-01-07 16:12:03 -0600

    resume incorporated and footer links with images added

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,14 +3,18 @@ import izzyImage from "../../assets/AboutMe/izzyImage.jpg";
 const About = () => (
   <div>
     <h1>About Me</h1>
+    <br></br>
     <img
       src={izzyImage}
       className="my-2"
       style={{ width: "25%", borderRadius: 300 }}
       alt="headshot"
     />
+     <br></br>
+     <br></br>
+     <br></br>
     <p>
-      Hola! My name is Izzy Ramirez and I am a full-stack web developer with a
+    &nbsp;&nbsp;&nbsp;Hola! My name is Izzy Ramirez and I am a full-stack web developer with a
       science, music, and art background. In other words, I make cool programmer
       things inspired by the world around me. I am open for interesting projects
       and collaborations.
