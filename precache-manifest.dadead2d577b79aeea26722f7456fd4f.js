e4f4a5eb9a1e635a4e6028701323a4ac2b6fa8ff
Ethan Chandler <86173119+IzzTinkr@users.noreply.github.com>
2022-01-08 16:12:18 -0600

    Updates

---
+++ precache-manifest.dadead2d577b79aeea26722f7456fd4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccbe969a86c9b4ca9479b9077e4d75e6",
+    "url": "/IsabelR-Portfolio/index.html"
+  },
+  {
+    "revision": "1784e8fd22897ede6f03",
+    "url": "/IsabelR-Portfolio/static/css/main.8ae786ee.chunk.css"
+  },
+  {
+    "revision": "6e0079fbc021ca331aad",
+    "url": "/IsabelR-Portfolio/static/js/2.f5cf3ae7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/IsabelR-Portfolio/static/js/2.f5cf3ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1784e8fd22897ede6f03",
+    "url": "/IsabelR-Portfolio/static/js/main.edf7eb02.chunk.js"
+  },
+  {
+    "revision": "6cbe07760e3dfc5e62eb",
+    "url": "/IsabelR-Portfolio/static/js/runtime-main.89aafdb3.js"
+  },
+  {
+    "revision": "3990822986ff7694e6d99d081e9b4aff",
+    "url": "/IsabelR-Portfolio/static/media/isabelResume.39908229.pdf"
+  },
+  {
+    "revision": "98bcc7f9d583235686e60df5b0fc5e6f",
+    "url": "/IsabelR-Portfolio/static/media/izzyImage.98bcc7f9.jpg"
+  }
+]);
